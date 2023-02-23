726cecf10e66339806ba25a323acfa3529eed311
Sebastian Baar <mail.sebastianbaar@gmail.com>
2023-02-23 20:45:23 +0100

    Initial Commit

---
+++ SBQuickLook_Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SBQuickLook_Example
+//
+//  Created by Sebastian Baar on 23.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SBQuickLook_Example/SBQuickLook_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBQuickLook_ExampleApp.swift
+//  SBQuickLook_Example
+//
+//  Created by Sebastian Baar on 23.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SBQuickLook_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
